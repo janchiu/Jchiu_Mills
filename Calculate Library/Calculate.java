2c864b31427adc553df1dc24c03a3dd6545e5428
janchiu <17chiuj39@millshighsch.org>
2016-08-30 09:26:43 -0700

    method

---
+++ Calculate Library/Calculate.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author APCS1
+ *
+ */
+public class Calculate {
+	public static double square(double number) {
+		return(number*number);
+	}
+
+}
+
+	public static double cube(double number) {
+		return(number*number*number);
+	}
+	public static double average(double number) {
+		return()
+	}
