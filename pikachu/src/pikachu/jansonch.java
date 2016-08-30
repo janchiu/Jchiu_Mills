5454351a2942a94e1d8beceac81a4bbb1c1a7444
janchiu <17chiuj39@millshighsch.org>
2016-08-30 08:31:46 -0700

    test code
    test code

---
+++ pikachu/src/pikachu/jansonch.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package pikachu;
+
+/**
+ * @author APCS1
+ *Date: August 30,2016
+ *TestCodde program -- used to show how to create a jar file that I can then submit on classroom 
+ */
+public class jansonch {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
