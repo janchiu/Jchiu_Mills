c377c6c8f6e2339efdbf40e4d973b2344a876061
janchiu <17chiuj39@millshighsch.org>
2016-08-30 11:32:36 -0700

    Changes for calculate

--- Calculate Library/Calculate.java
+++ Calculate Library/Calculate.java
@@ -16,6 +16,27 @@ public class Calculate {
 	public static double cube(double number) {
 		return(number*number*number);
 	}
-	public static double average(double number) {
+	public static double average(double number1, double number2) {
+		return((number1+number2)/2);
+	}
+	
+	public static double average(double number1, double number2, double number3) {
+		return((number1+number2+number3)/3);
+	}
+	
+	public static double toDegrees(double number) {
+		double pi= 3.14159;
+		return((number*360)/pi);
+	}
+	
+	public static double toRadians(double number) {
+		double pi= 3.14159;
+		return((number/360)*pi);
+	}
+	
+	public static double discriminant(double nunmber1, double number2, double number3) {
+		return()
+				
+	public static int toMixedNum(int number1, int number2, int number3)
 		return()
 	}
