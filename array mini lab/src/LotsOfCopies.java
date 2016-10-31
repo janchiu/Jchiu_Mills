ccfeab0fe8c9f8cdc5ecc4683df8df68322e9936
janchiu <17chiuj39@millshighsch.org>
2016-10-31 08:45:43 -0700

    commit
    arrays mini lab

---
+++ array mini lab/src/LotsOfCopies.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author APCS1
+ *
+ */
+import java.util.*;
+import java.util.Arrays.*;
+public class LotsOfCopies {
+	public static void main(String[] args) {
+		int num = 7;
+		String strMain = "APCS";
+		int[] arrMain = {1,2,3,4,5};
+		System.out.println(Arrays.toString(arrMain));
+		System.out.println(num);
+		System.out.println(strMain);
+	}
+	//public static void changeMe(int x, String str, int[] arr) {
+		//int[] a = startValue;
+		//int b = a;
+		//int a = newValue;
+	//}
+}
